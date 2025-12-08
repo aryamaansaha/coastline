f052e08eb9918f0b803a071144f306d08d8ee65e
Aryamaan Saha <aryamaansaha78@gmail.com>
2025-12-08 10:05:41 -0500

    feat: create index file to export core components for easier imports

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,6 @@
+export { ActivityCard } from './ActivityCard';
+export { DaySection } from './DaySection';
+export { BudgetBar } from './BudgetBar';
+export { DiscoveryDrawer } from './DiscoveryDrawer';
+export { RevisionModal } from './RevisionModal';
+
